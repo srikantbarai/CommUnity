d523004725fd7129491e564769ef75f50a0be310
Srikanta Barai <sbarai3210@gmail.com>
2025-07-08 12:01:30 +0530

    msg

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/api"
-
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL,
     withCredentials: true
 });
