a5ff1930f823260d4cc48c81955d55a9d8e8dadc
Srikanta Barai <sbarai3210@gmail.com>
2025-07-10 21:14:44 +0530

    Loop fix

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -9,8 +9,8 @@ export const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
   res => res,
   err => {
-    if (err.response?.status === 401) {
-      window.location.href = "/login";
+    if (err.response?.status === 401 && window.location.pathname !== '/login') {
+        window.location.href = '/login';
     }
     return Promise.reject(err);
   }
